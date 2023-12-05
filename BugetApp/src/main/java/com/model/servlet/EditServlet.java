dbfc43b5374570c4005ad6e6c170e10c88bc5124
Mrigya Sahai <sahaimrigya@gmail.com>
2023-12-06 00:00:30 +0530

    added new project folder

---
+++ BugetApp/src/main/java/com/model/servlet/EditServlet.java
@@ -0,0 +1,67 @@
+package com.model.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/editurl")
+public class EditServlet extends HttpServlet {
+    private static final String query = "update ITEMS set ITEM_NAME=?,ITEM_DESCRIPTION=?,ITEM_PRICE=? where id=?";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        //get PrintWriter
+        PrintWriter pw = res.getWriter();
+        //set content type
+        res.setContentType("text/html");
+        //get the id of record
+        int id = Integer.parseInt(req.getParameter("id"));
+        //get the edit data we want to edit
+        String itemName = req.getParameter("bookName");
+        String itemDescription = req.getParameter("itemDescription");
+        float itemPrice = Float.parseFloat(req.getParameter("itemPrice"));
+        //LOAD jdbc driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException cnf) {
+            cnf.printStackTrace();
+        }
+        //generate the connection
+        try (Connection con = DriverManager.getConnection("jdbc:mysql:///purchases", "root", "tiger"); 
+        	PreparedStatement ps = con.prepareStatement(query);) {
+            ps.setString(1, itemName);
+            ps.setString(2, itemDescription);
+            ps.setFloat(3, itemPrice);
+            ps.setInt(4, id);
+            int count = ps.executeUpdate();
+            if (count == 1) {
+                pw.println("<h2>Record is Edited Successfully</h2>");
+            } else {
+                pw.println("<h2>Record is not Edited Successfully</h2>");
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+            pw.println("<h1>" + se.getMessage() + "</h2>");
+        } catch (Exception e) {
+            e.printStackTrace();
+            pw.println("<h1>" + e.getMessage() + "</h2>");
+        }
+        pw.println("<a href='home.html'>Home</a>");
+        pw.println("<br>");
+        pw.println("<a href='bookList'>Book List</a>");
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+}
---
+++ BugetApp/src/main/java/com/model/servlet/RegisterServlet.java
@@ -0,0 +1,62 @@
+package com.model.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    private static final String query = "INSERT INTO ITEMS(ITEM_NAME,ITEM_DESCRIPTION,ITEM_PRICE) VALUES(?,?,?)";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        //get PrintWriter
+        PrintWriter pw = res.getWriter();
+        //set content type
+        res.setContentType("text/html");
+        //GET THE book info
+        String itemName = req.getParameter("itemName");
+        String itemDescription = req.getParameter("itemDescription");
+        float itemPrice = Float.parseFloat(req.getParameter("itemPrice"));
+        //LOAD jdbc driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException cnf) {
+            cnf.printStackTrace();
+        }
+        //generate the connection
+        try (Connection con = DriverManager.getConnection("jdbc:mysql:///purchases", "root", "tiger"); 
+        	PreparedStatement ps = con.prepareStatement(query);) {
+            ps.setString(1, itemName);
+            ps.setString(2, itemDescription);
+            ps.setFloat(3, itemPrice);
+            int count = ps.executeUpdate();
+            if (count == 1) {
+                pw.println("<h2>Record Is Registered Sucessfully</h2>");
+            } else {
+                pw.println("<h2>Record not Registered Sucessfully");
+            }
+        } catch (SQLException se) {
+            se.printStackTrace();
+            pw.println("<h1>" + se.getMessage() + "</h2>");
+        } catch (Exception e) {
+            e.printStackTrace();
+            pw.println("<h1>" + e.getMessage() + "</h2>");
+        }
+        pw.println("<a href='home.html'>Home</a>");
+        pw.println("<br>");
+        pw.println("<a href='bookList'>Book List</a>");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req, res);
+    }
+}
